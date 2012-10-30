4a3a3a2f2335aad558adb14a31ed418d45894d5c
Paolo Cosentino <cosepaol88@gmail.com>
2012-10-30 13:00:49 -0500

    contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,9 @@ class PagesController < ApplicationController
 	def send_email
 		first_name = params[:user][:first_name]
 		last_name = params[:user][:last_name]
-		raise (first_name + " " + last_name).inspect
+		email_address = params[:user][:email_address]
+		notes = params[:user][:notes]
+		raise (first_name + " " + last_name + " " + email_address + " " + notes).inspect
 		#UserMailer.send_email(:description => ..., :first_name => ...)
 	end
 
