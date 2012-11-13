1b6d1ef9c77c2a05cd426900da58a68e81584642
Paolo Cosentino <cosepaol88@gmail.com>
2012-11-13 12:24:46 -0600

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,7 @@ class PagesController < ApplicationController
 
 
 	def send_email
-		user_name =  "#{params[:user][:first_name]} <br /> #{params[:user][:last_name]}"
-		#user_name = params[:user][:first_name] + " " + params[:user][:last_name]
+		user_name =  "#{params[:user][:first_name]} #{params[:user][:last_name]}"
 		email_address = params[:user][:email_address]
 		notes = params[:user][:notes]
 		UserMailer.send_email({:name => user_name, :email => email_address, :description => notes}).deliver
