d9e57e489ecde5e280040dfb57afa156b3f10e08
Paolo Cosentino <cosepaol88@gmail.com>
2012-11-17 20:07:32 -0600

    logo

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -59,7 +59,11 @@ function showCaption(){
 
 
 
-
+$(document).ready(function() {
+    var controller = $.superscrollorama();
+    controller.addTween('#fade', 
+      TweenMax.from($('#fade'), .5, {css:{opacity:0}}));
+  });
 
 
 
