0417762760f42e67a494989c90b4491d293d8625
Paolo Cosentino <cosepaol88@gmail.com>
2012-12-03 12:52:50 -0600

    intro moved to js

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,20 +1,23 @@
 
-
-
-
-$(document).ready(function() {
-    var controller = $.superscrollorama();
-    controller.addTween('#fade', 
-      TweenMax.from($('#fade'), .5, {css:{opacity:0}}));
-  });
-
-
-
-
-
-
-
 $(document).ready(function(){
+
+
+
+        (function hereWeGo() { 
+            var positions = {
+                     front: {scale:1, left:"10%", top:"10%"},
+                    };
+            var boxes = [$('[data-pos="front"]')];
+            
+            //drop things into place (intro)
+            for (var i = 0; i < boxes.length; i++) {
+              TweenLite.from(boxes[i], .3, {css:{opacity:0}, delay:i * 1.1 + 0.25});
+              TweenLite.fromTo(boxes[i], 1, {css:{top:"-50%"}}, {css:positions[boxes[i].attr("data-pos")], delay:i * .1 + 1.0, ease:Elastic.easeOut, immediateRender:true});
+            }
+          })();
+
+
+
 
   				$('.lightbox').live('click',function(){
   				  var this_lightbox = $(this).siblings("#box_content");
@@ -24,22 +27,16 @@ $(document).ready(function(){
   					$('.box').animate({'opacity':'1.00'}, 300, 'linear');
   					$('.backdrop, .box').css('display', 'block');
   				});
-
   				$('.close').click(function(){
   					close_box();
   				});
-
   				$('.backdrop').click(function(){
   					close_box();
   				});
-
   			});
 
-  			
 
-
-
-            function close_box()
+        function close_box()
   			{
   				$('.backdrop, .box').animate({'opacity':'0'}, 300, 'linear', function(){
   					$('.backdrop, .box').css('display', 'none');
@@ -50,28 +47,28 @@ $(document).ready(function(){
 
   			
   			function ScrollToBottom() {
-            $('html,body').animate({
-                   scrollTop: $('#btnScrollToTop').offset().top}, 2000);
-            }
+          $('html,body').animate({
+                 scrollTop: $('#btnScrollToTop').offset().top}, 2000);
+          }
 
-            function ScrollToPositionA() {
-            $('html,body').animate({
-                    scrollTop: $('#portfolio').offset().top}, 1000);
-            }
-            
-            function ScrollToPositionB() {
-            $('html,body').animate({
-                      scrollTop: $('#about').offset().top}, 1000);
-            }
-           
-            function ScrollToPositionC() {
-            $('html,body').animate({
-                  scrollTop: $('#contact').offset().top}, 1000);
-            }
+          function ScrollToPositionA() {
+          $('html,body').animate({
+                  scrollTop: $('#portfolio').offset().top}, 1000);
+          }
+          
+          function ScrollToPositionB() {
+          $('html,body').animate({
+                    scrollTop: $('#about').offset().top}, 1000);
+          }
+         
+          function ScrollToPositionC() {
+          $('html,body').animate({
+                scrollTop: $('#contact').offset().top}, 1000);
+          }
 
-            function ScrollToTop() {
-            $('html,body').animate({scrollTop: 0}, 1000);
-            }
+          function ScrollToTop() {
+          $('html,body').animate({scrollTop: 0}, 1000);
+          }
   		
 
 
