2eaf2574835f51ce09378ff4f9818ed031fe8c68
Paolo Cosentino <cosepaol88@gmail.com>
2013-06-06 14:30:09 -0500

    change admin features

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -3,6 +3,6 @@ ActionMailer::Base.smtp_settings = {
 	:port                 => 587,
 	:domain               => "gmail.com",
 	:user_name            => 'pcportfolio88@gmail.com',
-	:password             => 'gr33nlight',
+	:password             => 'ilovelucy12',
 	:authentication       => 'plain',
 	:enable_starttls_auto => true  }
