a347ab90807fa6d9ae335d6b2e8b3649c8dcffa1
Paolo Cosentino <cosepaol88@gmail.com>
2015-09-03 14:59:51 -0500

    update portfolio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,12 @@ class PagesController < ApplicationController
 	def index
 	end
 
-
-
 	def send_email
-		user_name =  "#{params[:user][:first_name]} #{params[:user][:last_name]}"
-		email_address = params[:user][:email_address]
-		notes = params[:user][:notes]
-		UserMailer.send_email({:name => user_name, :email => email_address, :description => notes}).deliver
-		respond_to :js
+	    user_name =  "#{params[:user][:first_name]} #{params[:user][:last_name]}"
+	    email_address = params[:user][:email_address]
+	    notes = params[:user][:notes]
+	    UserMailer.send_email({:name => user_name, :email => email_address, :description => notes}).deliver
+	    respond_to :js
 	end
 
 end
